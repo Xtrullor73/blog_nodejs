f10d1039686fe878e85b335117ce34545f72d944
Xtrullor <xtrullor@bk.ru>
2023-07-13 21:50:40 -0700

    Integrated EJS

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = new express()
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
     console.log('hosted');
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve((__dirname), 'pages/index.html'))
+    res.render('index');
 })
 
 app.get('/post.html', (req, res) => {
-    res.sendFile(path.resolve((__dirname), 'pages/post.html'))
+    res.render('post');
 })
 
 app.get('/contact.html', (req, res) => {
-    res.sendFile(path.resolve((__dirname), 'pages/contact.html'))
+    res.render('contact');
 })
 
 app.get('/about.html', (req, res) => {
-    res.sendFile(path.resolve((__dirname), 'pages/about.html'))
+    res.render('about');
 })
