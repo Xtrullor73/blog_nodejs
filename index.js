b6fc67865244d7b248de66ae49e74cb0c36475d8
Xtrullor <xtrullor@bk.ru>
2023-07-15 17:14:22 -0700

    Added new post functionality

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const BlogPost = require('./models/BlogPost');
 
-const app = new express()
+mongoose.connect('mongodb+srv://xtrullor73:Dkflbr73@cluster.uvstdm5.mongodb.net/', {useNewUrlParser: true});
+
+const app = new express();
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
@@ -15,14 +23,26 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.get('/post.html', (req, res) => {
+app.get('/post', (req, res) => {
     res.render('post');
 })
 
-app.get('/contact.html', (req, res) => {
+app.get('/contact', (req, res) => {
     res.render('contact');
 })
 
-app.get('/about.html', (req, res) => {
+app.get('/about', (req, res) => {
     res.render('about');
 })
+
+app.get('/post/new', (req, res) => {
+    res.render('create');
+})
+
+app.post('/posts/store', (req, res) => {
+    BlogPost.create(req.body).then(r => {
+        console.log(r);
+        res.redirect('/');
+    });
+})
+
